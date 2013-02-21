4c1078f0130f6af34621164d4f2b6949a2e21cac
MusiqueLibre <dev@dogmazic.net>
2013-02-21 15:00:33 +0100

    rename yiiuser

--- yiiuser/protected/views/site/index.php
+++
@@ -1,2 +0,0 @@
-<p>Cette page vous permet de vous inscrire et de gérer votre inscription pour tout les sites de dogmazic.</p>
-<p>Pour le moment, ça vous permet d'accéder seulement au forum, mais quand dogma sera complet vous verrez à quel point c'est pratique !</p>
