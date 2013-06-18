0b44d612554ebe1559fa56295c05ccb8e7905cb8
MusiqueLibre <dev@dogmazic.net>
2013-06-18 21:04:11 +0200

    commit before ftp push

--- inc/global_statics.php
+++ inc/global_statics.php
@@ -1,7 +1,7 @@
 <script type="text/javascript" src="../global_statics/js/jquery-2.0.1.min.js"></script>
 
-<link rel="stylesheet" type="text/css" href="../global_statics/css/normalize.css">
-<link rel="stylesheet.css" type="text/css" href="../global_statics/css/global.css">
+<link rel="stylesheet" type="text/css" href="global_statics/css/normalize.css">
+<link rel="stylesheet.css" type="text/css" href="global_statics/css/global.css">
 <?php //include the LESS JS processor here ?>
 <script type="text/javascript">
   $(function(){
