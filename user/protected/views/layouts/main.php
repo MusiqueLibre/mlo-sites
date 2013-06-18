0521a556698e17d4e6b12a4faef71a15eb39d94c
MusiqueLibre <dev@dogmazic.net>
2013-06-18 22:21:31 +0200

    commit before ftp push

--- user/protected/views/layouts/main.php
+++ user/protected/views/layouts/main.php
@@ -5,14 +5,6 @@
   <?php include('../inc/global_statics.php'); ?>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-  <script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
