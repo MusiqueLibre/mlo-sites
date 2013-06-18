84566057f780679bd148dfdd732a70317cf4826f
MusiqueLibre <dev@dogmazic.net>
2013-06-18 21:32:26 +0200

    commit before ftp push

--- user/protected/config_example/main.php
+++ user/protected/config_example/main.php
@@ -88,7 +88,6 @@ return array(
                     
 
 		'urlManager'=>array(
-			'urlFormat'=>'path',
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
