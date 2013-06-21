05eef7d627d96478a10c7a7c243deebda4bc4c4c
MusiqueLibre <dev@dogmazic.net>
2013-06-21 21:02:56 +0200

    commit before ftp push

--- wp-content/plugins/mlo_sso_authent/mlo_sso_authent.php
+++ wp-content/plugins/mlo_sso_authent/mlo_sso_authent.php
@@ -24,16 +24,13 @@ function logOnIfSso(){
     $session_id = mysql_query($query);
     $result = mysql_result($session_id, 0);
 
-    echo "TEST !!";
     echo $result;
     //IF there is a user, and the token in the cookie matches the one in the DB : logon !
     if($_COOKIE['sso_authent_mlo']['token'] == $result){
       wp_set_auth_cookie($user_id);
-    echo "START !!";
     }
   }elseif(is_user_logged_in() && !$user_id){
     //logout user without cookie
-    echo "STOP !!";
     wp_clear_auth_cookie();
   }
 }
