3bc4f3ff3ee5ac088692f6f9ccadd2b07810d605
MusiqueLibre <dev@dogmazic.net>
2013-06-23 18:16:28 +0200

    updating sso plugin for wp so it disconnect the user

--- forum/themes/dogma/views/default.master.php
+++ forum/themes/dogma/views/default.master.php
@@ -2,10 +2,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-ca">
 <head>
+   <?php include '../inc/global_statics.php'; ?>
+   <?php include '../inc/general_menu.php'; ?>
    <?php $this->RenderAsset('Head'); ?>
 </head>
 <body id="<?php echo $BodyIdentifier; ?>" class="<?php echo $this->CssClass; ?>">
-<?php include '../inc/generalmenu.php'; ?>
+
    <div id="Frame">
       <div id="Head">
          <div class="Menu">
