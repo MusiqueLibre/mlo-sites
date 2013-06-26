26d9427cc2d98967ec457706c9aafbae1711d3ab
MusiqueLibre <dev@dogmazic.net>
2013-06-26 12:04:25 +0200

    commit before ftp push

--- user/protected/modules/user/controllers/RecoveryController.php
+++ user/protected/modules/user/controllers/RecoveryController.php
@@ -36,8 +36,9 @@ class RecoveryController extends Controller
 								$find->save();
                 //copy the changes in other database
                 Yii::app()->copyvalues->updatePassword($find->username, $encryptedPW, $salt);
-
+                echo "I'm here";
 								$this->redirect(Yii::app()->controller->module->recoveryUrl);
+                die;
 							}
 						} 
 						$this->render('changepassword',array('form'=>$form2));
