79a25f08204af632afea4496ef4aab7c1159b181
MusiqueLibre <dev@dogmazic.net>
2013-07-03 16:17:43 +0200

    commit before ftp push

--- inc/vanilla_statics.php
+++ inc/vanilla_statics.php
@@ -1,8 +1,8 @@
-<link rel="stylesheet/less" type="text/css" href="/global_statics/css/global_vanilla.less">
+<link rel="stylesheet" type="text/css" href="/global_statics/css/global_vanilla.css">
 <script  type="text/javascript" src="/global_statics/js/vanilla/jquery.popup.js"></script>
 <script  type="text/javascript" src="/global_statics/js/vanilla/jquery.gardenhandleajaxform.js"></script>
 <script  type="text/javascript" src="/global_statics/js/vanilla/jquery.autocomplete.js"></script>
 <script  type="text/javascript" src="/global_statics/js/vanilla/jquery.livequery.js"></script>
 <script  type="text/javascript" src="/global_statics/js/vanilla/global.js"></script>
 <script  type="text/javascript" src="/global_statics/js/vanilla/conversations.js"></script>
-<?php include "less_proccess.php"; ?>
+<?php //include the LESS JS processor here ?>
