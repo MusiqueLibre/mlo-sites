f15cdba570c81d6dcbffa550c0a60659d9c6fcf2
MusiqueLibre <dev@dogmazic.net>
2013-09-05 15:40:37 +0200

    commit before ftp push

--- inc/global_logo.php
+++ inc/global_logo.php
@@ -1,4 +1,4 @@
 <div id="logo_box">
-  <img src="../global_statics/images/logo_inv.png" id="logo_box_img" alt="main logo">
+  <img src="../global_statics/images/logo_inv.png" class="logo_box_img" alt="main logo">
   <span id="slogan">militant|musics</span>
 </div>
