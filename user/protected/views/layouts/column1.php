d2b9ed7f91f305a4c2722af90081937edc9fccf5
MusiqueLibre <dev@dogmazic.net>
2014-10-06 17:02:36 +0200

    commiting some improvements

--- user/protected/views/layouts/column1.php
+++ user/protected/views/layouts/column1.php
@@ -1,6 +1,6 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
-<div id="full_content_container" class="content">
+<div id="full_content_container">
 	<?php echo $content; ?>
 </div><!-- content -->
 <?php $this->endContent(); ?>
